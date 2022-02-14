dfb2fb66a8e177c59121307bf73ce5452bafaaa8
SergiyRybin <serhiobax@gmail.com>
2022-02-14 17:51:59 +0200

    upd 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function(event) {
+
+
+console.log(event.seconds)
+    });
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+    // "videoplayer-current-time"
