195ffaaab764ad9c97955ce5093df25cd60630ca
SergiyRybin <serhiobax@gmail.com>
2022-02-21 19:39:06 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+import { get } from 'lodash';
 import throttle from 'lodash.throttle';
 
 const feedbackFormEmail = document.querySelector('.feedback-form')
@@ -5,19 +6,27 @@ const textArea = document.querySelector(".feedback-form textarea")
 const emailData = document.querySelector(".feedback-form input")
 const LOCAL_KEY = "feedback-form-state";
 
-const formData = {};
+let formData = {};
 
 feedbackFormEmail.addEventListener("input", throttle(onText, 500))
 feedbackFormEmail.addEventListener("submit", onSubmite)
 
 
-function onText(event){
-    formData[event.target.name]=event.target.value
-    const message = JSON.stringify(formData)
-    localStorage.setItem(LOCAL_KEY, message);
 
-    JSON.parse(localStorage.getItem(LOCAL_KEY))
-    console.log(JSON.parse(localStorage.getItem(LOCAL_KEY)))
+function onText(event){
+    
+    formData[event.target.name]=event.target.value
+    let message = JSON.stringify(formData)
+    localStorage.setItem(LOCAL_KEY, message)
+    console.log(formData)
+}
+
+if(localStorage.getItem(LOCAL_KEY)){
+    formData = JSON.parse(localStorage.getItem(LOCAL_KEY))
+    textArea.value = formData.message || "";
+    emailData.value = formData.email || "";
+    console.log(formData)
+
 }
 
 
@@ -26,16 +35,4 @@ function onSubmite(ev){
 ev.currentTarget.reset()
 localStorage.removeItem(LOCAL_KEY)
 }
-
-
-function saveData(){
-    const saveMessage = JSON.parse(localStorage.getItem(LOCAL_KEY))
-    if(saveMessage){
-        emailData.value = saveMessage.email || "";
-        textArea.textContent = saveMessage.message || "";
-    } 
-}
-saveData()
-
-
 
